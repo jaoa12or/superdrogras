a457328984b8e3aa969f83ba5bb31cf3d60025c7
jaoa12or <jhon.orobio@correounivalle.edu.co>
2019-08-13 13:23:41 -0500

    fix

--- apps/inventory/models.py
+++ apps/inventory/models.py
@@ -4,7 +4,8 @@ from apps.items.models import Product
 from django.utils.timezone import now
 
 class Inventory(BaseModel):
-    item = models.ForeignKey(to=Product,on_delete=models.CASCADE,blank=False,related_name='inventory',verbose_name='Item')
+    #item = models.ForeignKey(to=Product,on_delete=models.CASCADE,blank=False,related_name='inventory',verbose_name='Item')
+    item = models.IntegerField(blank=False,null=True,verbose_name='Cantidad Existente')
     existing_quantity = models.IntegerField(blank=False,null=True,verbose_name='Cantidad Existente')
     committed_quantity = models.IntegerField(blank=False,null=True,verbose_name='Cantidad Comprometida')
     last_entry_date = models.DateField(blank=False,null=True,verbose_name='Fecha Ultima Entrada')
