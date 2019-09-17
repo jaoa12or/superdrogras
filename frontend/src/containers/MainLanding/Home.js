d2daeddf1652156e091e9b552db5050bc1c875db
jaoa12or <jhon.orobio@correounivalle.edu.co>
2019-09-17 18:40:10 -0500

    landing franquicas

--- frontend/src/containers/MainLanding/Home.js
+++ frontend/src/containers/MainLanding/Home.js
@@ -4,7 +4,7 @@ import Home from '../../components/MainLanding/Home';
 import FranchiseHome from '../../components/FranchiseLanding/FranchiseHome';
 import Franchise from '../../components/Franchise/Franchise';
 const franchise = new Franchise();
-const hostname = window.location.hostname;
+const hostname = window.location.host;
 const subdomain = hostname.split('.');
 class MainLandin extends React.Component {
 
