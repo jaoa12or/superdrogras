ea92a9ab78bee94b427ecf456eec6a0480cb0fef
jaoa12or <jhon.orobio@correounivalle.edu.co>
2019-09-17 17:35:14 -0500

    landing franquicas

--- apps/franchise/serializers.py
+++ apps/franchise/serializers.py
@@ -2,7 +2,7 @@
 
 from rest_framework import serializers
 from .models import Franchise
-
+# serializer utilizado para las franquicias
 class FranchiseSerializer(serializers.ModelSerializer):
   class Meta:
     model = Franchise
